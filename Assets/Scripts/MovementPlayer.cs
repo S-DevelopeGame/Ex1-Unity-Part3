e618f7ba5842172f6a016da6392cfe3baab05838
snir1551 <snir1552@gmail.com>
2021-11-02 18:17:16 +0200

    update

--- Assets/Scripts/MovementPlayer.cs
+++ Assets/Scripts/MovementPlayer.cs
@@ -6,7 +6,6 @@ public class MovementPlayer : MonoBehaviour
 {
     [Tooltip("speed movement of the object")]
     [SerializeField] private float speed;
-    private Vector3 movement;
     [SerializeField] private Animator animator;
     private bool rightFace = true;
     private float oldSpeed = 0;
@@ -20,48 +19,42 @@ public class MovementPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-
-        if(Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.UpArrow) || (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.LeftArrow)))
+        {
+            animator.SetBool("isWalking", false);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow) && transform.position.x < 10.300)
         {
             transform.position += Vector3.right * Time.deltaTime * speed;
             animator.SetBool("isWalking", true);
         }
-        else if (Input.GetKey(KeyCode.LeftArrow))
+        else if (Input.GetKey(KeyCode.UpArrow) && transform.position.y < -0.6)
+        {
+            transform.position += Vector3.up * Time.deltaTime * speed;
+            animator.SetBool("isWalking", true);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow) && transform.position.y > -4.2)
+        {
+            transform.position += Vector3.down * Time.deltaTime * speed;
+            animator.SetBool("isWalking", true);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow) && transform.position.x > -10.300)
         {
             transform.position += Vector3.left * Time.deltaTime * speed;
             animator.SetBool("isWalking", true);
         }
-        else
-        {
-            animator.SetBool("isWalking", false);
-        }
 
-        if(!rightFace && Input.GetKeyDown(KeyCode.RightArrow))
+
+
+        if (!rightFace && Input.GetKeyDown(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow))
         {
             rightFace = true;
             transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
         }
-            
-        else if(rightFace && Input.GetKeyDown(KeyCode.LeftArrow))
+        else if (rightFace && Input.GetKeyDown(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow))
         {
             rightFace = false;
             transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
-        }
-          
-
-
-
-
-
-
-        if (transform.position.x < -10.300 && !rightFace || transform.position.x > 10.300 && rightFace)
-        {
-            speed = 0;
-        }
-        else
-        {
-            speed = oldSpeed;
         }
 
 
--- Assets/Scripts/MovementPlayer2.cs
+++ Assets/Scripts/MovementPlayer2.cs
@@ -7,9 +7,11 @@ public class MovementPlayer2 : MonoBehaviour
     [Tooltip("speed movement of the object")]
     [SerializeField] private float speed;
     private Vector3 movement;
+    [Tooltip("the Animator of the player")]
     [SerializeField] private Animator animator;
-    private bool rightFace = true;
-    private float oldSpeed = 0;
+
+    private bool rightFace; // the player is looking to the right
+    private float oldSpeed = 0; // save the speed of the player
     // Start is called before the first frame update
     void Start()
     {
@@ -22,47 +24,44 @@ public class MovementPlayer2 : MonoBehaviour
     {
 
 
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.W) || (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.A)))
+        {
+            animator.SetBool("isWalking", false);
+        }
+        if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A) && transform.position.x < 10.300)
         {
             transform.position += Vector3.right * Time.deltaTime * speed;
             animator.SetBool("isWalking", true);
         }
-        else if (Input.GetKey(KeyCode.A))
+        else if (Input.GetKey(KeyCode.W) && transform.position.y < -0.6)
+        {
+            transform.position += Vector3.up * Time.deltaTime * speed;
+            animator.SetBool("isWalking", true);
+        }
+        else if (Input.GetKey(KeyCode.S) && transform.position.y > -4.2)
+        {
+            transform.position += Vector3.down * Time.deltaTime * speed;
+            animator.SetBool("isWalking", true);
+        }
+        else if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D) && transform.position.x > -10.300)
         {
             transform.position += Vector3.left * Time.deltaTime * speed;
             animator.SetBool("isWalking", true);
         }
-        else
-        {
-            animator.SetBool("isWalking", false);
-        }
 
-        if (!rightFace && Input.GetKeyDown(KeyCode.D))
+
+
+        if (!rightFace && Input.GetKeyDown(KeyCode.D) && !Input.GetKey(KeyCode.A))
         {
             rightFace = true;
             transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
         }
-
-        else if (rightFace && Input.GetKeyDown(KeyCode.A))
+        else if (rightFace && Input.GetKeyDown(KeyCode.A) && !Input.GetKey(KeyCode.D))
         {
             rightFace = false;
             transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
         }
 
-
-
-
-
-
-
-        if (transform.position.x < -10.300 && !rightFace || transform.position.x > 10.300 && rightFace)
-        {
-            speed = 0;
-        }
-        else
-        {
-            speed = oldSpeed;
-        }
 
 
 
